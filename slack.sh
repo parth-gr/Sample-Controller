7c32d95f0a3e7578dc4bb02a0c5825cf5a887c7c
parth-gr <paarora@redhat.com>
2021-10-07 16:29:44 +0530

    add your own slack webhook
    Signed-off-by: parth-gr

--- slack.sh
+++ slack.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"pod metadat changed"}' https://hooks.slack.com/services/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
+# add your webhook
+# example https://hooks.slack.com/services/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
+YourWebhook=
+curl -X POST -H 'Content-type: application/json' --data '{"text":"pod metadata changed"}' $YourWebhook
